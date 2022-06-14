23ec1da911b98e668329abb7315b871f323e6d82
omerktk <greatomer876@gmail.com>
2022-06-14 15:45:34 +0500

    1

---
+++ ProjectAPI/ProjectAPI/Models/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectAPI.Models
+{
+    public class Class1
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string pro { get; set; }
+        public string phone { get; set; }
+        public string photo { get; set; }
+    }
+}
